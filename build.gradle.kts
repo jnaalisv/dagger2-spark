4a6a5f90fcfdecfd61371964602372eeedcd3a05
Juho Naalisvaara <juho.naalisvaara@gmail.com>
2018-06-20 16:46:45 +0300

    almost working lombok

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,12 @@ plugins {
     java
     id ("net.ltgt.apt") version "0.16"
     id ("net.ltgt.apt-idea") version "0.16"
+    id ("io.franzbecker.gradle-lombok") version "1.14"
     application
+}
+
+lombok {
+    version = "1.18.0"
 }
 
 application {
@@ -13,12 +18,15 @@ repositories {
     jcenter()
 }
 
+val lombokVersion = "1.18.0"
 val sparkVersion = "2.7.2"
 val daggerVersion = "2.16"
 
 dependencies {
 
     annotationProcessor ("com.google.dagger:dagger-compiler:$daggerVersion")
+    annotationProcessor ("org.projectlombok:lombok:$lombokVersion")
+
     compile ("com.sparkjava:spark-core:$sparkVersion")
     compile ("com.google.dagger:dagger:$daggerVersion")
 
