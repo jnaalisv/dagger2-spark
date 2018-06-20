99e54a5a330e025e226dbd9ed66ac00aea39a17d
Juho Naalisvaara <juho.naalisvaara@gmail.com>
2018-06-20 13:13:46 +0300

    add and configure logback

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,4 +9,5 @@ repositories {
 dependencies {
 
     compile ("com.sparkjava:spark-core:2.7.2")
+    runtime ("ch.qos.logback:logback-classic:1.2.3")
 }
--- src/main/java/org/example/SparkApplication.java
+++ src/main/java/org/example/SparkApplication.java
@@ -1,13 +1,18 @@
 package org.example;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import static spark.Spark.get;
 import static spark.Spark.port;
 
 public class SparkApplication {
+    private static final Logger logger = LoggerFactory.getLogger(SparkApplication.class);
 
     public static void main(String[] args) {
         port(4567);
         get("/hello", (request, response) -> {
+            logger.info("hello");
             return "Hello, World";
         });
     }
